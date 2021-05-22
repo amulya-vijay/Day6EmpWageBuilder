e01dc6900b62f6381d5841ccbb94415a4523c1f2
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-22 20:26:58 +0530

    [amulya] switch case is implemented to calculate wages of different employees

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -25,7 +25,7 @@ class DailyWage{
 	}
 	public String dailyWage() { //method to calculate dailywage of en employee
 		daily_wage=this.wage_per_hr*this.hrs_per_day;
-		return "Daily wage of an Employee : "+daily_wage;
+		return "Daily wage of full-time Employee : "+daily_wage;
 	}
 }
 
@@ -34,6 +34,7 @@ class PartTime{
 	private final int wage_per_hr;
 	private final int emphrs;
 	public int parttime_wage;
+	
 	public PartTime(int wage,int hrs) {
 		this.wage_per_hr=wage;
 		this.emphrs=hrs;
@@ -45,17 +46,41 @@ class PartTime{
 		return "Daily wage of part time employee is : "+parttime_wage;
 	}
 }
+class SwitchEmployee{
+	
+	static int empHrs;
+	private static final int full_time_hrs=8;
+	private static final int part_time_hrs=4;
+
+	public static int empCheck= (int) Math.floor(Math.random()*2+1); //generates a random vallue in the range (1,2)
+	
+	public static void switchEmp() { //method to check if an employee is full-time or part-time
+		
+		switch(empCheck) { 
+		case 1: //calculates daily wage of full-time employee
+			empHrs=8;
+			DailyWage dw = new DailyWage(100,empHrs); //Object of Daily wage class
+			System.out.println(dw.dailyWage()); //call to daily wage calculation method
+			break; 
+		case 2: //calculates daily wage of part-time employee
+			empHrs=4;
+			PartTime pt = new PartTime(80,empHrs); //Object of Part Time class
+			System.out.println(pt.part_time_wage()); //call to part time wage calculation method
+			break;
+		default:
+			break;
+		}
+	}
+}
 public class EmployeeWage {
+	
 	public static void main(String [] args) {
 		System.out.println("Welcome to Employee Wage Computation"); //prints welcome message
 		
 		Attendance att = new Attendance(); //object of attendance class
 		att.emp_attendance(); // call to attendance method through object 
 		
-		DailyWage dw = new DailyWage(100,8); //Object of Daily wage class
-		System.out.println(dw.dailyWage()); //call to daily wage calculation method
+		SwitchEmployee.switchEmp(); // call to employee switch method
 		
-		PartTime pt = new PartTime(80,4); //Object of Part Time class
-		System.out.println(pt.part_time_wage()); //call to part time wage calculation method
 	}
 }
