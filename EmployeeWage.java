41aef00383a6e14df49422090fd0de61b7c440f2
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-22 20:00:49 +0530

    [amulya] daily wage of part time employee is implemented

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -29,6 +29,22 @@ class DailyWage{
 	}
 }
 
+class PartTime{
+	
+	private final int wage_per_hr;
+	private final int emphrs;
+	public int parttime_wage;
+	public PartTime(int wage,int hrs) {
+		this.wage_per_hr=wage;
+		this.emphrs=hrs;
+	}
+	
+	String part_time_wage() {
+		
+		parttime_wage=this.wage_per_hr*this.emphrs;
+		return "Daily wage of part time employee is : "+parttime_wage;
+	}
+}
 public class EmployeeWage {
 	public static void main(String [] args) {
 		System.out.println("Welcome to Employee Wage Computation"); //prints welcome message
@@ -38,5 +54,8 @@ public class EmployeeWage {
 		
 		DailyWage dw = new DailyWage(100,8); //Object of Daily wage class
 		System.out.println(dw.dailyWage()); //call to daily wage calculation method
+		
+		PartTime pt = new PartTime(80,4); //Object of Part Time class
+		System.out.println(pt.part_time_wage()); //call to part time wage calculation method
 	}
 }
