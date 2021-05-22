fea6136697c12b276f9f97cca53168f776528a40
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-22 19:47:58 +0530

    [amulya] method to calculate daily wage of an employee is implemented

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -13,11 +13,30 @@ class Attendance{
 	}
 }
 
+class DailyWage{
+	
+	private final int wage_per_hr;
+	private final int hrs_per_day;
+	public static int daily_wage;
+	
+	public DailyWage(int wage, int hrs) { //constructor : initializes the variables
+		this.wage_per_hr=wage;
+		this.hrs_per_day=hrs;
+	}
+	public String dailyWage() { //method to calculate dailywage of en employee
+		daily_wage=this.wage_per_hr*this.hrs_per_day;
+		return "Daily wage of an Employee : "+daily_wage;
+	}
+}
+
 public class EmployeeWage {
 	public static void main(String [] args) {
 		System.out.println("Welcome to Employee Wage Computation"); //prints welcome message
 		
 		Attendance att = new Attendance(); //object of attendance class
 		att.emp_attendance(); // call to attendance method through object 
+		
+		DailyWage dw = new DailyWage(100,8); //Object of Daily wage class
+		System.out.println(dw.dailyWage()); //call to daily wage calculation method
 	}
 }
